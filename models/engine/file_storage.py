19b14e4adf6f07470e908bd515ce1abaec0d822d
Ilvian <d.ilvian1993@gmail.com>
2023-10-30 14:40:04 -0700

    task 4

---
+++ models/engine/file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        objdict = {}
+        for k, v in FileStorage.__objects.items():
+            objdict[k] = v.to_dict()
+        with open(FileStorage.__file_path, "w") as jsonFile:
+            json.dump(objdict, jsonFile)
+
+    def reload(self):
+        with open(FileStorage.__file_path, "r") as jsonFile:
+            objdict = json.load(jsonFile)
