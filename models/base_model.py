b86459ed0030eed17880c790cdb96a92336c2ec8
Ilvian <d.ilvian1993@gmail.com>
2023-10-31 16:19:01 -0700

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -4,42 +4,26 @@ This script defines a BaseModel class for managing and persisting data.
 """
 
 
-import models
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """
-    Initialize a new BaseModel instance.
-    """
-    def __init__(self, *args, **kwargs):
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            models.storage.new(self)
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def save(self):
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Convert the object into a dictionary representation.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
 
     def __str__(self):
-        """
-        Return a string representation of the object.
-        """
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
