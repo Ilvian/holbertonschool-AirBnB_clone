6f01de562bebeacce9879c9f0d691f569c3f3281
Ilvian <d.ilvian1993@gmail.com>
2023-10-31 09:50:40 -0700

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This script defines a BaseModel class for managing and persisting data.
+"""
 
 
 import models
@@ -7,7 +10,15 @@ from datetime import datetime
 
 
 class BaseModel:
+    """
+    Initialize a new BaseModel instance.
+    Args:
+        *args: Non-keyword arguments (not used in this implementation).
+        **kwargs: Keyword arguments, typically used for object initialization.
 
+    If **kwargs is provided, it updates the object's attributes based on the keyword arguments.
+    If **kwargs is not provided, it initializes the object with default values and adds it to the storage system.
+    """
     def __init__(self, *args, **kwargs):
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) != 0:
@@ -23,10 +34,19 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
+        """
+        Update the "updated_at" attribute with the current timestamp and save the object.
+        """
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
+        """
+        Convert the object into a dictionary representation.
+
+        Returns:
+            dict: A dictionary containing the object's attributes and class name.
+        """
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
@@ -34,5 +54,11 @@ class BaseModel:
         return rdict
 
     def __str__(self):
+        """
+        Return a string representation of the object.
+
+        Returns:
+            str: A string containing the object's class name, ID, and attributes.
+        """
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
