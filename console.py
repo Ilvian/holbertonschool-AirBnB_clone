fcc8f51081096386b84cc34ca123e7156d3f5e0a
Ilvian <d.ilvian1993@gmail.com>
2023-10-31 09:06:03 -0700

    task 6

--- console.py
+++ console.py
@@ -5,23 +5,45 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    Custom command interpreter class.
+    """
     prompt = "(hbnb) "
 
     def emptyline(self):
+        """
+        Do nothing when an empty line is entered.
+        """
         pass
 
     def do_quit(self, arg):
+        """
+        Exit the program.
+        """
         return True
 
     def do_EOF(self, arg):
+        """
+        Exit the program when Ctrl+D (EOF) is entered.
+        """
         return True
 
     def help_quit(self):
+        """
+        Provide help for the 'quit' command.
+        """
         print("Quit command to exit the program")
 
     def help_EOF(self):
+        """
+        Provide help for the 'EOF' command.
+        """
         print("EOF command to exit the program.")
 
 
 if __name__ == '__main__':
+    """
+    Check if the code is being executed directly (not imported).
+    Create an instance of the HBNBCommand class and start the command loop.
+    """
     HBNBCommand().cmdloop()
