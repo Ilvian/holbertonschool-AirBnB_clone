04fb7b20203f5d1ae64dbd6ee9ce605047267468
Ilvian <d.ilvian1993@gmail.com>
2023-10-31 08:48:12 -0700

    task 6

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, arg):
+        return True
+
+    def do_EOF(self, arg):
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
