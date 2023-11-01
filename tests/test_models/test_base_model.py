d03a6262d344fecfe3973f7e11ac366d5e02f319
Ilvian <d.ilvian1993@gmail.com>
2023-11-01 11:08:09 -0700

    test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,33 @@
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        model = BaseModel()
+        self.assertIsNotNone(model.id)
+        self.assertIsNotNone(model.created_at)
+        self.assertIsNotNone(model.updated_at)
+
+    def test_save(self):
+        model = BaseModel()
+        initial_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(initial_updated_at, model.updated_at)
+
+    def test_to_dict(self):
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertIn('__class__', model_dict)
+        self.assertIn('created_at', model_dict)
+        self.assertIn('updated_at', model_dict)
+
+    def test_str(self):
+        model = BaseModel()
+        model_str = str(model)
+        self.assertIsInstance(model_str, str)
+        self.assertTrue(model.__class__.__name__ in model_str)
+        self.assertTrue(model.id in model_str)
+
+if __name__ == '__main__':
+    unittest.main()
