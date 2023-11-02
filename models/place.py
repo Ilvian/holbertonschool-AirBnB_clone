59a844fc7e62ce77ebabfcd98f935e9ef45d9e85
Ilvian <d.ilvian1993@gmail.com>
2023-11-02 11:19:26 -0700

    tasks

--- models/place.py
+++ models/place.py
@@ -8,6 +8,9 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
     city_id = ""
     user_id = ""
     name = ""
@@ -21,4 +24,8 @@ class Place(BaseModel):
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         super().__init__(*args, **kwargs)
--- models/review.py
+++ models/review.py
@@ -8,9 +8,16 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
     place_id = ""
     user_id = ""
     text = ""
 
     def __init__(self, *args, **kwargs):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         super().__init__(*args, **kwargs)
