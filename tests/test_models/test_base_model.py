16bbe045bce0c66903176bbf65cca24e6882aa3b
Ilvian <d.ilvian1993@gmail.com>
2023-11-03 02:51:18 -0700

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,35 +1,85 @@
+#!/usr/bin/python3
+'''
+This script initializes a FileStorage instance and reloads data,
+making it available for managing and persisting data.
+'''
+
+
+import io
+import sys
 import unittest
+import os
+import datetime
 from models.base_model import BaseModel
+from models import storage
 
 
 class TestBaseModel(unittest.TestCase):
-    def test_init(self):
-        model = BaseModel()
-        self.assertIsNotNone(model.id)
-        self.assertIsNotNone(model.created_at)
-        self.assertIsNotNone(model.updated_at)
+    '''
+    This script initializes a FileStorage instance and reloads data,
+    making it available for managing and persisting data.
+    '''
+
+    def test_attributes(self):
+        '''
+        This script initializes a FileStorage instance and reloads data,
+        making it available for managing and persisting data.
+        '''
+
+        base1 = BaseModel()
+        base2 = BaseModel()
+        self.assertNotEqual(base1.id, base2.id)
+        self.assertNotEqual(base1.created_at, base2.created_at)
+        self.assertNotEqual(base1.updated_at, base2.updated_at)
+
+    def test_attribute_type(self):
+        '''
+        This script initializes a FileStorage instance and reloads data,
+        making it available for managing and persisting data.
+        '''
+
+        base1 = BaseModel()
+        self.assertEqual(type(base1.id), str)
+        self.assertEqual(type(base1.created_at), datetime.datetime)
+        self.assertEqual(type(base1.updated_at), datetime.datetime)
+
+    def test_storage(self):
+        '''
+        This script initializes a FileStorage instance and reloads data,
+        making it available for managing and persisting data.
+        '''
+
+        base = BaseModel()
+        self.assertNotEqual(len(storage.all()), 0)
 
     def test_save(self):
-        model = BaseModel()
-        initial_updated_at = model.updated_at
-        model.save()
-        self.assertNotEqual(initial_updated_at, model.updated_at)
+        '''
+        This script initializes a FileStorage instance and reloads data,
+        making it available for managing and persisting data.
+        '''
+
+        base = BaseModel()
+        time = base.save()
+        self.assertEqual(base.updated_at, time)
 
     def test_to_dict(self):
-        model = BaseModel()
-        model_dict = model.to_dict()
-        self.assertIsInstance(model_dict, dict)
-        self.assertIn('__class__', model_dict)
-        self.assertIn('created_at', model_dict)
-        self.assertIn('updated_at', model_dict)
+        '''
+        This script initializes a FileStorage instance and reloads data,
+        making it available for managing and persisting data.
+        '''
 
-    def test_str(self):
-        model = BaseModel()
-        model_str = str(model)
-        self.assertIsInstance(model_str, str)
-        self.assertTrue(model.__class__.__name__ in model_str)
-        self.assertTrue(model.id in model_str)
+        base = BaseModel()
+        new_d = base.to_dict()
+        self.assertEqual(new_d['id'], base.id)
+        self.assertEqual(new_d['created_at'], base.created_at.isoformat())
+        self.assertEqual(new_d['updated_at'], base.updated_at.isoformat())
 
+    def test_to_str(self):
+        '''
+        This script initializes a FileStorage instance and reloads data,
+        making it available for managing and persisting data.
+        '''
 
-if __name__ == '__main__':
-    unittest.main()
+        base = BaseModel()
+        x = base
+        self.assertEqual(base, x)
